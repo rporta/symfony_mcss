c7e3b39934f16dcdde54be63ea3efd2eca36e8ba
Ramiro Portas <rporta@teceng-gaming.com.ar>
2018-09-04 14:27:50 -0300

    se agrega toast a obj a

--- src/AppBundle/Resources/views/default/index.html.php
+++ src/AppBundle/Resources/views/default/index.html.php
@@ -176,6 +176,13 @@ $pag = new AppBundle\Utility\Obj\pag($pag);
 
 	$main->addObj($card);
 
+			$a_fff['toast'] = 'fafafa';
+			$a_fff['class'] = 'btn';
+			$a_fff['textColor'] = 'grey,9';
+			$a_fff['text'] = 'btn 1';
+			$a_fff = new AppBundle\Utility\Obj\a($a_fff);
+	
+	$main->addObj($a_fff);
 
 	$footer['shadow'] = 5;
 	$footer['backgroundColor'] = "red,6";
