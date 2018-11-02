d392e225fc9bae6905c423bd2c1976558464e607
Ramiro Portas <rporta@teceng-gaming.com.ar>
2018-11-02 17:12:42 -0300

    se modifican controladores se agrega index method

--- src/AppBundle/Controller/HiddenAjaxAddController.php
+++ src/AppBundle/Controller/HiddenAjaxAddController.php
@@ -3,7 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Utility\dirbase\dirBase;
 
 class HiddenAjaxAddController extends Controller
 {
+    public function indexAction(Request $request)
+    {
+    	
+    }	
 }
--- src/AppBundle/Controller/HiddenAjaxModController.php
+++ src/AppBundle/Controller/HiddenAjaxModController.php
@@ -3,7 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Utility\dirbase\dirBase;
 
 class HiddenAjaxModController extends Controller
 {
+    public function indexAction(Request $request)
+    {
+    	
+    }
 }
