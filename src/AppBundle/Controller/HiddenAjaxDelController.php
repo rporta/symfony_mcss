a412ff0411c886283621cbdd6c4dc5988a9a803f
Ramiro Portas <rporta@teceng-gaming.com.ar>
2018-12-03 12:22:59 -0300

    se termina de desarrollar del obj hidden

--- src/AppBundle/Controller/HiddenAjaxDelController.php
+++ src/AppBundle/Controller/HiddenAjaxDelController.php
@@ -26,36 +26,33 @@ class HiddenAjaxDelController extends Controller
             $pathListObj = str_replace("/web", $relativePathListObj, $request->server->get('DOCUMENT_ROOT'));
 
         }
-        //traigo obj add    
-        $data = $request->request->all();
-
         $serviceObj = $this->container->get('obj');
         $post['editar_pagina'] = $request->attributes->get('pag');
-        $dirbase2 = new dirbase($pathListObj);
 
         $dirbase = new dirbase($path);
         $file = $dirbase->getObj($post['editar_pagina']);
         $filePag = $file->viewFile();
         unset($dirbase);
+        
         //traigo obj pag
         $objPag = $serviceObj->scanObjFile($filePag);
+
+        $dirbase2 = new dirbase($pathListObj);
+
         //traigo obj disponibles
         $objDir = $serviceObj->scanObjDir($dirbase2);
         unset($dirbase2);
 
-        $objAdd = $serviceObj->jsonSetParam($data['json']);
-
-        $nameObjAdd = $serviceObj->getName($objAdd, $objPag, $objDir);
-
-        $tempElement['mode'] = "listobjhidden";
-        $tempElement['action'] = "/ajaxadd2/{$post['editar_pagina']}";
-        $tempElement['objDir'] = $objDir;
+        $objHiddenPag = $serviceObj->scanHiddenObj($objPag);
+        
+        $tempElement['mode'] = "hiddenobjmod";
+        $tempElement['action'] = "/hiddenajaxprocessdel/{$post['editar_pagina']}";
+        $tempElement['objPag'] = $objPag;
         $tempElement['editar_pagina'] = $post['editar_pagina'];
-
-        return $this->render('AppBundle:default:newObj.html.php' ,array('tempElement' => $tempElement));
+        return $this->render('AppBundle:default:newObj.html.php' ,array('tempElement' => $tempElement));        
     }
     public function delAction(Request $request){
-        die();
+
         if(strpos($request->server->get('DOCUMENT_ROOT'), "/") === FALSE ){ 
             #path plantillas html Windows
             $relativePathListObj = "\\src\\AppBundle\\Utility\\Obj";
@@ -72,11 +69,11 @@ class HiddenAjaxDelController extends Controller
             $pathListObj = str_replace("/web", $relativePathListObj, $request->server->get('DOCUMENT_ROOT'));
 
         }
-        //traigo obj del	
-    	$data = $request->request->all();
+        //traigo obj del    
+        $data = $request->request->all();
 
         $serviceObj = $this->container->get('obj');
-    	$post['editar_pagina'] = $request->attributes->get('pag');
+        $post['editar_pagina'] = $request->attributes->get('pag');
 
         $dirbase = new dirbase($path);
         $file = $dirbase->getObj($post['editar_pagina']);
@@ -84,25 +81,13 @@ class HiddenAjaxDelController extends Controller
         unset($dirbase);
         //traigo obj pag
         $objPag = $serviceObj->scanObjFile($filePag);
-        // xbug($objPag);
 
-        $dirbase2 = new dirbase($pathListObj);
-        //traigo obj disponibles
-        $objDir = $serviceObj->scanObjDir($dirbase2);
-        unset($dirbase2);
-     
-        $objDel = $serviceObj->jsonSetParam($data['json']);
-
-        $nameObjDel = $serviceObj->getName($objDel, $objPag, $objDir);
-
-        $objPag = $serviceObj->delObj($nameObjDel, $objPag);
+        $objPag = $serviceObj->delObj($data['name'], $objPag);
 
         $codePhp = $serviceObj->createPhp($objPag);
         
         $file->truncateFile();
         $file->editFile($codePhp);
-
-        
-        return $this->json("objeto ({$nameObjDel}) eliminado con exito");    	
+        return $this->redirectToRoute('edit_pag', array('pag' => $post['editar_pagina']));
     }
 }
